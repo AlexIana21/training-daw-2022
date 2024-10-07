7cdd01cc4f226e6bffa1ca36172727fb38ea2541
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-07 16:46:35 +0200

    ejercicios

--- UD1/11_even_numbers/main.js
+++ UD1/11_even_numbers/main.js
@@ -1,6 +1,10 @@
-function evenNumbers() {
 
+function imprimirPares(){
+    for (var i=0; i<101; i++){
+      if (i%2 === 0) {
+        console.log(i)
+      }
+    }
 }
 
-evenNumbers();
-// expected output 2,4,6,8,10......98
+imprimirPares ()
