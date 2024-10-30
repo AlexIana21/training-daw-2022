2ebbb2265397c019d228dd6d6acfb8c57a487e0e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-30 20:00:22 +0100

    30/10/24

--- UD3/07_events/main.js
+++ UD3/07_events/main.js
@@ -1,3 +1,28 @@
-window.onload = function(e) {
-    console.log('documento cargado')
+window.addEventListener("load", function() {
+    console.log('documento cargado');
+    
+    // Obtener enlaces 
+    const enlaces = document.querySelectorAll('[id^="enlace_"]');
+    
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("click", function(event) {
+            event.preventDefault();
+            toggleContent(enlace);
+        });
+    });
+});
+
+function toggleContent(enlace) {
+
+    const numero = enlace.id.split("_")[1];
+    const contenido = document.getElementById(`contenidos_${numero}`);
+    
+    if (contenido.style.display === "none") {
+        contenido.style.display = "block";
+        enlace.innerHTML = "Ocultar contenidos";
+    } else {
+        contenido.style.display = "none";
+        enlace.innerHTML = "Mostrar contenidos";
+    }
 }
